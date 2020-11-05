546c33357e50a323eb4dc3149b96ec3dd185620b
Megan Russell <meganannerussell@gmail.com>
2020-11-05 19:28:37 +0200

    Add hello world

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const pool = require("./db")
+const pool = require("./db");
 
-const incomeRouter = require('./routers/incomes')
-const expenseRouter = require('./routers/expenses')
+const incomeRouter = require("./routers/incomes");
+const expenseRouter = require("./routers/expenses");
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.use(cors())
-app.use(express.json()) //req.body
-app.use(incomeRouter, expenseRouter)
+app.use(cors());
+app.use(express.json()); //req.body
+app.use(incomeRouter, expenseRouter);
 
-
-app.listen(port, ()=>{
-    console.log("server has started on port" + port)
-} )
+app.listen(port, () => {
+  console.log("server has started on port" + port);
+});
